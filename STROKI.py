15c0aa4e7312f27e85dcf5e2a90b5df80db93528
BAMB11 <90822130+BAMB11@users.noreply.github.com>
2021-11-10 22:35:19 +0500

    Add files via upload

---
+++ STROKI.py
@@ -0,0 +1,23 @@
+a = 'leg right monitor far'
+b = a.split()
+print(len(b))
+
+#(hello -> h*ll*)
+
+a = 'right monitor'
+for x in a:
+    if x in "АаЕеIiOoUuYy":
+        a = a.replace(x,'*')
+        
+print(a)
+
+#выбросить из нее все слова короче 4 букв
+
+a = 'leg right monitor far'
+b = a.split()
+c = []
+
+for a in b:
+    if len(a)>4:
+        c.append(a)
+print(c)
